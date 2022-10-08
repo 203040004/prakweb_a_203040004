f379f1d7d88b798ce1a2d8debec49b0feafd479d
hasbulahshopari <hasbullahhasbu01@gmail.com>
2022-10-08 17:56:33 +0700

    menyelesaikan vidio 5 #View

--- MVC/app/controllers/About.php
+++ MVC/app/controllers/About.php
@@ -1,12 +1,24 @@
 <?php
 
-class About {
-    public function index($nama = 'hasbulah', $pekerjaan = 'mahasiswa')
-    {
-        echo "Halo, nama saya $nama, saya adalah $pekerjaan";
-    }
-    public function page()
-    {
-        echo 'About/page';
-    }
+class About extends Controller
+{
+
+  public function index($nama = 'Aril', $pekerjaan = 'Mahasiswa', $umur = 20)
+  {
+    $data['nama'] = $nama;
+    $data['pekerjaan'] = $pekerjaan;
+    $data['umur'] = $umur;
+    $data['judul'] = 'About';
+    $this->view('templates/header', $data);
+    $this->view('about/index', $data);
+    $this->view('templates/footer');
+  }
+
+  public function page()
+  {
+    $data['judul'] = 'Page';
+    $this->view('templates/header', $data);
+    $this->view('about/page');
+    $this->view('templates/footer');
+  }
 }
